3e9892211f95776a02a5009640a9fa66aacf5b07
suhasab <suhasbalagar@gmail.com>
2022-10-10 13:27:38 -0700

    Updates

---
+++ precache-manifest.8f2d874b8aec5ed271a5052defce5705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "25f6678100e859cea6c7",
+    "url": "/about-suhas-reactapp/static/css/main.b4e90042.chunk.css"
+  },
+  {
+    "revision": "25f6678100e859cea6c7",
+    "url": "/about-suhas-reactapp/static/js/main.d4f0057a.chunk.js"
+  },
+  {
+    "revision": "661bac2637f9454f84d8",
+    "url": "/about-suhas-reactapp/static/js/runtime~main.3c5a5d10.js"
+  },
+  {
+    "revision": "c58cb75a7feaf5003ac1",
+    "url": "/about-suhas-reactapp/static/js/2.e6637c1e.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/about-suhas-reactapp/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/about-suhas-reactapp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "715d5fa5762be650850f6f48b575813e",
+    "url": "/about-suhas-reactapp/index.html"
+  }
+];
