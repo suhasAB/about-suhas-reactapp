cc53d31980789ad83874a613024a2c80b8e5aebf
Suhas Anand Balagar <suhasbalagar@gmail.com>
2022-10-10 13:02:23 -0700

    first deployment of react-about-me-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my page</h1>
+      <h3>Suhas Anand Balagar</h3>
+       <img src={logo} alt="that's me" />
+    </div>
+  );
+}
+
+export default App;
